9789f9d5a26631bdfbf3f5a81dbf49636e4fc623
kongou <oukong84@gmail.com>
2020-03-02 23:08:36 +0700

    Implementing Toggle complete checkbox

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
       {
         id: 2,
         title: 'Dinner with wife',
-        completed: false
+        completed: true
       },
       {
         id: 3,
@@ -23,10 +23,22 @@ class App extends React.Component {
     ]
   };
 
+  // Toggle Complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
   render() {
     return (
       <div className='App'>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    };
+  };
+
   render() {
+    const { id, title } = this.props.todo;
     return (
-      <div>
-        <p>{this.props.todo.title}</p>
+      <div style={this.getStyle()}>
+        <p>
+          <input type='checkbox' onChange={this.props.markComplete.bind(this, id)} /> {title}
+        </p>
       </div>
     );
   }
 }
+
+// PropTypes
+TodoItem.propType = {
+  todo: PropTypes.object.isRequired
+};
 
 export default TodoItem;
