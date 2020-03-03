c067e6351c62a5f3e0eeed9212d03e2d113ce572
kongou <oukong84@gmail.com>
2020-03-03 18:04:42 +0700

    create router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
@@ -40,7 +42,9 @@ class App extends React.Component {
 
   // Delete Todo
   delTodo = id => {
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
   };
 
   // Add Todo
@@ -55,13 +59,28 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className='App'>
-        <div className='container'>
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      <Router>
+        <div className='App'>
+          <div className='container'>
+            <Header />
+            <Route
+              exact
+              path='/'
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path='/about' component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
