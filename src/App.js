756b847e2cb8e69976cddad86080d8a14f6ce35b
kongou <oukong84@gmail.com>
2020-03-03 16:57:10 +0700

    Implementing Add Todo to List

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 class App extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: 'Dinner with wife',
         completed: true
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: 'Meeting with bose',
         completed: false
       }
@@ -35,14 +38,29 @@ class App extends React.Component {
     });
   };
 
+  // Delete Todo
   delTodo = id => {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  };
+
+  // Add Todo
+  addTodo = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
   };
 
   render() {
     return (
       <div className='App'>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        <div className='container'>
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
       </div>
     );
   }
