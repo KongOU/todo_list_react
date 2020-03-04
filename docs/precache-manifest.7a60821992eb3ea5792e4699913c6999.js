3e91b062e6fa60c11f583faf86aeb0a312e9935b
kongou <oukong84@gmail.com>
2020-03-04 19:39:57 +0700

    Create folder docs

---
+++ docs/precache-manifest.7a60821992eb3ea5792e4699913c6999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2974a6367b1af71fe626de063984dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c3c53fcda8bdb3b3bfa",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "c89bfa2717a78dcd366c",
+    "url": "/static/js/2.e3a7b816.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e3a7b816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3c53fcda8bdb3b3bfa",
+    "url": "/static/js/main.f96d120f.chunk.js"
+  },
+  {
+    "revision": "01ef1e69f125e2e9232f",
+    "url": "/static/js/runtime-main.d84cbb92.js"
+  }
+]);
